c0df1a1fc30f73d3d3b5dc17be651e7a78dbbd33
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-03-24 23:44:32 +0330

    add icon to the categories section

--- components/Categories.js
+++ components/Categories.js
@@ -13,15 +13,23 @@ const Categories = () => {
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">دسته بندی مطالب</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        دسته بندی مطالب
+      </h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
           <span
-          style={{direction:"ltr"}}
+            style={{ direction: "ltr" }}
             className={`cursor-pointer block ${
               index === categories.length - 1 ? "border-b-0" : "border-b"
             } pb-3 mb-3`}
           >
+            <img
+              className="inline mr-2"
+              width="26"
+              alt="React-icon"
+              src={category.slugIcon}
+            />
             {category.name}
           </span>
         </Link>
--- services/index.js
+++ services/index.js
@@ -83,6 +83,7 @@ export const getCategories = async () => {
       category {
         name
         slug
+        slugIcon
       }
     }
   `;
