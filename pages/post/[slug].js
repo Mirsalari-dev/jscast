530cc202a7fc7c58cf496eff79b1a2bc5edf9ffb
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-03-31 19:43:29 +0330

    add head section for title of posts

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -7,31 +7,38 @@ import CommentsForm from "../../components/CommentsForm";
 import Comments from "../../components/Comments";
 import PostWidget from "../../components/PostWidget";
 import Categories from "../../components/Categories";
-import useTitle from "../../helper/useTitle";
 
 const postDetails = ({ post }) => {
-  useTitle(post.title)
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="col-span-1 lg:col-span-8">
-          <PostDetail post={post} />
-          <Author author={post.author} />
-          <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
-          <CommentsForm slug={post.slug} />
-          <Comments slug={post.slug} />
-        </div>
-        <div className="col-span-1 lg:col-span-4">
-          <div className="relative lg:sticky top-8">
-            <PostWidget
-              slug={post.slug}
-              category={post.category.map((category) => category.slug)}
-            />
-            <Categories />
+    <>
+      <Head>
+        <title>{post.title}</title>
+        <link
+          rel="icon"
+          href="https://upload.wikimedia.org/wikipedia/commons/5/57/Code.svg"
+        />
+      </Head>
+      <div className="container mx-auto px-10 mb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+            <PostDetail post={post} />
+            <Author author={post.author} />
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
+          </div>
+          <div className="col-span-1 lg:col-span-4">
+            <div className="relative lg:sticky top-8">
+              <PostWidget
+                slug={post.slug}
+                category={post.category.map((category) => category.slug)}
+              />
+              <Categories />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
