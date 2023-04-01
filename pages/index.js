8fc400399b9eac2043bd675ac0ffe451b5943fc5
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-04-01 06:03:54 +0330

    remove useTitle hook and replace head nextjs component

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,9 @@ import PostCard from "../components/PostCard";
 import PostWidget from "../components/PostWidget";
 import { getPost } from "../services";
 import FeaturedPosts from "../sections/FeaturedPosts";
-import useTitle from "../helper/useTitle";
 import SkeletonPost from "../components/Skeleton/SkeletonPost";
 
-export default function Home({ posts }) {
+export default function Home({ posts, error }) {
   const [showModal, setShowModal] = useState(true);
 
   useEffect(() => {
@@ -17,8 +16,6 @@ export default function Home({ posts }) {
     }, 12000);
     return () => clearInterval(interval);
   }, [showModal]);
-
-  useTitle("جی اس کست");
 
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -29,22 +26,22 @@ export default function Home({ posts }) {
           href="https://upload.wikimedia.org/wikipedia/commons/5/57/Code.svg"
         />
       </Head>
-        <FeaturedPosts />
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 ">
-          <div className="lg:col-span-8 col-span-1">
-            {!posts && [1, 2, 3, 4].map((item) => <SkeletonPost key={item} />)}
-            {posts &&
-              posts.map((post) => (
-                <PostCard post={post.node} key={post.title} />
-              ))}
-          </div>
-          <div className="lg:col-span-4 col-span-1">
-            <div className="lg:sticky relative top-8">
-              <PostWidget />
-              <Categories />
-            </div>
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12 ">
+        <div className="lg:col-span-8 col-span-1">
+          {!posts ||
+            error && [(1, 2, 3, 4)].map((item) => <SkeletonPost key={item} />)}
+          {posts &&
+            !error &&
+            posts.map((post) => <PostCard post={post.node} key={post.title} />)}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
         </div>
+      </div>
     </div>
   );
 }
@@ -55,9 +52,9 @@ export async function getServerSideProps() {
     return {
       props: { posts },
     };
-  } catch (error) {
+  } catch (err) {
     return {
-      props: {},
+      error: err,
     };
   }
 }
