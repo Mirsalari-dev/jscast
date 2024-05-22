1e198c6cee64d0a6239e302dae4b873feca9fe23
AmirhosseinMirsalari <amir.tk7@gmail.com>
2024-05-22 15:47:39 -0500

    ci cd test

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,7 @@ export default function Home({ posts, error }) {
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
+            <p>test mikonam</p>
             <Categories />
           </div>
         </div>
